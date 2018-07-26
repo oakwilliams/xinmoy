a050bc0c636161b58863870415ea225ce05ebeb5
Oak Williams <oakwilliams@gmail.com>
2018-07-26 17:37:33 +0800

    Role

--- start.php
+++ start.php
@@ -28,6 +28,54 @@ function handle_exception($e) {
 }
 
 
+/**
+ * Start register.
+ *
+ * @param array $config config
+ */
+function start_register($config) {
+    if (empty($config)) {
+        throw new Exception('wrong config');
+    }
+
+    if (empty($config['register']['host']) || ($config['register']['port'] < 0)) {
+        throw new Exception('wrong host/port');
+    }
+
+    $register = new Register($config['register']['host'], $config['register']['port']);
+    $register->start();
+}
+
+
+/**
+ * Start server.
+ *
+ * @param array $config config
+ */
+function start_server($config) {
+    if (empty($config)) {
+        throw new Exception('wrong config');
+    }
+
+    if (empty($config['server']['host']) || ($config['server']['port'] < 0)) {
+        throw new Exception('wrong server host/port');
+    }
+
+    if (empty($config['server']['name'])) {
+        throw new Exception('wrong name');
+    }
+
+    if (empty($config['register']['host']) || ($config['register']['port'] < 0)) {
+        throw new Exception('wrong register host/port');
+    }
+
+    $server = new Server($config['server']['host'], $config['server']['port']);
+    $server->setName($config['server']['name']);
+    $server->setRegisterAddress($config['register']['host'], $config['register']['port']);
+    $server->start();
+}
+
+
 // Set exception handler.
 set_exception_handler('handle_exception');
 
@@ -43,37 +91,9 @@ if (empty($config['role'])) {
     throw new Exception('wrong role');
 }
 
-switch ($config['role']) {
-    // Start register.
-    case 'register':
-    if (empty($config['register']['host']) || ($config['register']['port'] < 0)) {
-        throw new Exception('wrong host/port');
-    }
-
-    $register = new Register($config['register']['host'], $config['register']['port']);
-    $register->start();
-    break;
-
-    // Start server.
-    case 'server':
-        if (empty($config['server']['host']) || ($config['server']['port'] < 0)) {
-            throw new Exception('wrong server host/port');
-        }
-
-        if (empty($config['server']['name'])) {
-            throw new Exception('wrong name');
-        }
-
-        if (empty($config['register']['host']) || ($config['register']['port'] < 0)) {
-            throw new Exception('wrong register host/port');
-        }
-
-        $server = new Server($config['server']['host'], $config['server']['port']);
-        $server->setName($config['server']['name']);
-        $server->setRegisterAddress($config['register']['host'], $config['register']['port']);
-        $server->start();
-        break;
-
-    default:
-        break;
+$function = "start_{$config['role']}";
+if (!function_exists($function)) {
+    throw new Exception('wrong role');
 }
+
+$function($config);
